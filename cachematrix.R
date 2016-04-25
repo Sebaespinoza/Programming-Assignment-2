93be9789a170b26b4e08e7b9f753e83b1d4c4911
Sebaespinoza <sebaespinoza@udec.cl>
2016-04-25 06:18:08 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,13 @@
 ## First, should see README.md 
+
+## This file will include two functions:
+## 1. makeCacheMatrix()
+## 2. cacheSolve()
+
+## The function makeCacheMatrix creates a "matrix" which also can cache its inverse.
+## This function contains the following functions:
+
+# set          set the value of a matrix
+# get          get the value of a matrix
+# setinver     set the cached value from the inverse of the matrix.
+# getinver     get the cached value from the inverse of the matrix.
